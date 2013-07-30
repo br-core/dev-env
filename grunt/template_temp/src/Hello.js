9d108e56824c427857ed8c07dc9913c5dbede61d
lester_hu <jwhu1024@gmail.com>
2013-07-30 13:25:19 +0800

    update

---
+++ grunt/template_temp/src/Hello.js
@@ -0,0 +1,11 @@
+function MyName() {
+    return "My name is Lester";
+}
+
+function Compare(number) {
+	return (number < 10) ? true : false;
+}
+
+function add(num1, num2) {
+	return num1 + num2;
+}
